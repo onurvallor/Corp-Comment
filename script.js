9690b1e89fd774d9c16095cd8a49659df357e028
Onur Vallor <onurvallor@gmail.com>
2024-01-06 20:48:06 -0500

    Fetch feedback from API.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const MAX_CHARS = 150;
 
-
 const textareaEl = document.querySelector('.form__textarea');
 const counterEl = document.querySelector('.counter');
 const formEl = document.querySelector('.form');
 const feedbackListEl = document.querySelector('.feedbacks');
 const submitBtnEl = document.querySelector(".submit-btn");
+const spinnerEl = document.querySelector('.spinner');
 
 //Counter Component
 const inputHandler = () => {
@@ -15,6 +15,19 @@ const inputHandler = () => {
 textareaEl.addEventListener('input', inputHandler);
 
 // FORM Component
+
+const showVisualIndicator = (textCheck) => {
+
+    const className = textCheck === 'valid' ? 'form--valid' : 'form--invalid';
+
+    formEl.classList.add(className);
+    
+    setTimeout(() => {
+        formEl.classList.remove(className);
+    }, 2000)
+
+}
+
 const submitHandler = (event) => {
     //prevent default browser action
     event.preventDefault();
@@ -23,19 +36,9 @@ const submitHandler = (event) => {
 
     //validate text
     if(text.includes("#") && text.length >= 5){
-        formEl.classList.add('form--valid');
-
-        setTimeout(() => {
-            formEl.classList.remove('form--valid');
-        }, 2000)
-
+        showVisualIndicator('valid');
     }else{
-        formEl.classList.add('form--invalid');
-
-        setTimeout(() => {
-            formEl.classList.remove('form--invalid');
-        }, 2000)
-        
+        showVisualIndicator('invalid');
         textareaEl.focus();
 
         return;
@@ -78,3 +81,38 @@ const submitHandler = (event) => {
 };
 
 formEl.addEventListener('submit', submitHandler);
+
+
+// FEEDBACK COMPONENT
+fetch('https://bytegrad.com/course-assets/js/1/api/feedbacks').then(response => {
+    if(!response.ok){
+        console.log("ERROR has occurred with fetching");
+        return;
+    }
+    return response.json();
+}).then(data => {
+    spinnerEl.remove();
+    console.log(data.feedbacks[5]);
+
+    data.feedbacks.forEach(feedbackItem => {
+        const feedbackItemHTML = `
+        <li class="feedback">
+            <button class="upvote">
+                <i class="fa-solid fa-caret-up upvote__icon"></i>
+                <span class="upvote__count">${feedbackItem.upvoteCount}</span>
+            </button>
+            <section class="feedback__badge">
+                <p class="feedback__letter">${feedbackItem.badgeLetter}</p>
+            </section>
+            <div class="feedback__content">
+                <p class="feedback__company">${feedbackItem.company}</p>
+                <p class="feedback__text">${feedbackItem.text}</p>
+            </div>
+            <p class="feedback__date">${feedbackItem.daysAgo === 0 ? "NEW" : `${feedbackItem.daysAgo}d`}</p>
+        </li>`;        
+        feedbackListEl.insertAdjacentHTML('beforeend', feedbackItemHTML);
+    });
+
+}).catch(error => {
+    feedbackListEl.textContent = `Failed to fetch feedback item. Error message ${error.message}`;
+});
